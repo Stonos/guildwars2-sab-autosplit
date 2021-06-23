729997c75da678380a1a5ecdd201f947bb3532d9
Mike Kosmatopoulos <stonos@gmail.com>
2021-06-24 01:33:17 +0300

    Add support for circle area type

--- LiveSplit.GW2SAB/Area.cs
+++ LiveSplit.GW2SAB/Area.cs
@@ -6,7 +6,7 @@ namespace LiveSplit.GW2SAB
     public struct Area
     {
         public AreaType AreaType { get; set; }
-        
+
         /// <summary>
         /// <code>
         /// p1         p2
@@ -18,29 +18,35 @@ namespace LiveSplit.GW2SAB
         /// </code>
         /// </summary>
         public Coordinates2[] Polygon { get; set; }
-        
+
+        public Coordinates2 Center { get; set; }
+
+        public double Radius { get; set; }
+
         public double MinimumHeight { get; set; }
 
         public bool IsPointInArea(Coordinates3 testPoint)
-        {
-            //TODO: Use polymorphism instead of doing this
-            switch (AreaType)
-            {
-                case AreaType.Polygon:
-                    return IsPointInPolygon(testPoint);
-                default:
-                    throw new Exception($"Unsupported AreaType {AreaType}");
-            }
-        }
-        
-        // https://stackoverflow.com/a/14998816/3210008
-        private bool IsPointInPolygon(Coordinates3 testPoint)
         {
             if (testPoint.Y < MinimumHeight)
             {
                 return false;
             }
 
+            //TODO: Use polymorphism instead of doing this
+            switch (AreaType)
+            {
+                case AreaType.Polygon:
+                    return IsPointInPolygon(testPoint);
+                case AreaType.Circle:
+                    return IsPointInCircle(testPoint);
+                default:
+                    throw new Exception($"Unsupported AreaType {AreaType}");
+            }
+        }
+
+        // https://stackoverflow.com/a/14998816/3210008
+        private bool IsPointInPolygon(Coordinates3 testPoint)
+        {
             var result = false;
             var j = Polygon.Length - 1;
             for (var i = 0; i < Polygon.Length; i++)
@@ -59,6 +65,16 @@ namespace LiveSplit.GW2SAB
             }
 
             return result;
+        }
+
+        private bool IsPointInCircle(Coordinates3 testPoint)
+        {
+            var dx = Math.Abs(testPoint.X - Center.X);
+            if (dx > Radius) return false;
+            var dy = Math.Abs(testPoint.Z - Center.Y);
+            if (dy > Radius) return false;
+            if (dx + dy <= Radius) return true;
+            return dx * dx + dy * dy <= Radius * Radius;
         }
     }
 }
--- LiveSplit.GW2SAB/AreaType.cs
+++ LiveSplit.GW2SAB/AreaType.cs
@@ -3,5 +3,6 @@
     public enum AreaType
     {
         Polygon,
+        Circle,
     }
 }
